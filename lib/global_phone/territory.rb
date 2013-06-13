2a49e2718e66d4941db5aa472ef7d2dee304a904
Richard Huang <flyerhzm@gmail.com>
2013-06-13 22:16:35 +0800

    check all number types in Territory#possible?

--- lib/global_phone/territory.rb
+++ lib/global_phone/territory.rb
@@ -22,8 +22,8 @@ module GlobalPhone
     field 16, :uan_national_pattern do |p| /^#{p}$/ end
     field 17, :voicemail_possible_pattern do |p| /^#{p}$/ end
     field 18, :voicemail_national_pattern do |p| /^#{p}$/ end
-    field 19, :fixedLine_possible_pattern do |p| /^#{p}$/ end
-    field 20, :fixedLine_national_pattern do |p| /^#{p}$/ end
+    field 19, :fixed_line_possible_pattern do |p| /^#{p}$/ end
+    field 20, :fixed_line_national_pattern do |p| /^#{p}$/ end
     field 21, :mobile_possible_pattern do |p| /^#{p}$/ end
     field 22, :mobile_national_pattern do |p| /^#{p}$/ end
     field 23, :national_prefix_formatting_rule
@@ -68,7 +68,26 @@ module GlobalPhone
       end
 
       def possible?(string)
-        string =~ general_desc_possible_pattern
+        :unknown != number_type(string)
+      end
+
+      def number_type(string)
+        return :unknown if !general_desc_national_pattern || !number_matching_desc?(string, general_desc_possible_pattern, general_desc_national_pattern)
+        return :premium_rate if number_matching_desc?(string, premium_rate_possible_pattern, premium_rate_national_pattern)
+        return :toll_free if number_matching_desc?(string, toll_free_possible_pattern, toll_free_national_pattern)
+        return :shared_cost if number_matching_desc?(string, shared_cost_possible_pattern, shared_cost_national_pattern)
+        return :voip if number_matching_desc?(string, voip_possible_pattern, voip_national_pattern)
+        return :personal_number if number_matching_desc?(string, personal_number_possible_pattern, personal_number_national_pattern)
+        return :pager if number_matching_desc?(string, pager_possible_pattern, pager_national_pattern)
+        return :uan if number_matching_desc?(string, uan_possible_pattern, uan_national_pattern)
+        return :voicemail if number_matching_desc?(string, voicemail_possible_pattern, voicemail_national_pattern)
+        return :fixed_line if number_matching_desc?(string, fixed_line_possible_pattern, fixed_line_national_pattern)
+        return :mobile if number_matching_desc?(string, mobile_possible_pattern, mobile_national_pattern)
+        :unknown
+      end
+
+      def number_matching_desc?(string, possible_pattern, national_pattern)
+        string =~ (possible_pattern ? possible_pattern : general_desc_possible_pattern) && string =~ national_pattern
       end
 
       def starts_with_national_prefix?(string)
